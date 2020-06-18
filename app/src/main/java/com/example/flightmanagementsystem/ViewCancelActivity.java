164cdb748c11c958c099f1465226f12d03a7eb23
Muhammad Ismail <iyaqoob62@gmail.com>
2020-06-19 01:04:59 +0500

    Added cancel view activity

--- app/src/main/java/com/example/flightmanagementsystem/ViewCancelActivity.java
+++ app/src/main/java/com/example/flightmanagementsystem/ViewCancelActivity.java
@@ -1,7 +1,6 @@
 package com.example.flightmanagementsystem;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
 public class ViewCancelActivity extends AppCompatActivity {
